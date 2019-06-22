cdc2d5e060aa01a8a9c6716f8c579bca23f416a8
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-06-22 20:11:55 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_favorite_food_to_artists.rb
+++ db/migrate/02_add_favorite_food_to_artists.rb
@@ -1,4 +1,4 @@
-class AddFavoriteFoodToArtists < ActiveRecord::Migration
+class AddFavoriteFoodToArtists < ActiveRecord::Migration[4.2]
   def change
     add_column :artists, :favorite_food, :string
   end
